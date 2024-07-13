6c656c3262f042abea7b645e49f3cb6d1427d1fa
Sanchi-Chaurasia <rimjhimchaurasia22@gmail.com>
2024-07-14 01:16:12 +0530

    first commit

---
+++ src/Components/Community/Posts/Post.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div className='flex justify-center w-screen'>
+        <div className='border border-gray-500 rounded-xl'>
+            <div className='bg-black min-h-48 w-50'>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Post
---
+++ src/Components/Community/community.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Post from './Posts/Post'
+function Community() {
+  return (
+    <div>
+        <Post/>
+    </div>
+  )
+}
+
+export default Community
